c68f06450f40f397501ed07d5eb89e9025d5be5b
+ <+>
2022-12-11 21:10:11 +0300

    fetching data from the call API

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
 const input = document.getElementById('input');
 const searchBtn=document.getElementById('btn');
+const notFoundWord=document.querySelector('.cant_find_word');
+const apiKey='';
 
 searchBtn.addEventListener('click',e =>{
-    console.log('Hello World!!')
+    // console.log('Hello World!!')
     e.preventDefault();
 
     const searchedWord=input.value;
-    console.log(searchedWord);
-    if (searchedWord==="");
+    // console.log(searchedWord);
+    if (searchedWord===""){
     alert('Please enterd the searched Word');
     return;
-})
+    }
+    
+    getSearchedWord(searchedWord);
+});
+
+async function getSearchedWord(searchedWord){
+    const response = await fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${searchedWord}?key=696bb10c-9957-46a7-a71d-ebdc6368e23d`);
+    const data = await response.json();
+    console.log(data);
+    
+    
+
+}
