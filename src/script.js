cad4cabc18a95729e781c9cc2a3f72815160c74b
+ <+>
2022-12-11 21:31:33 +0300

    insert the message for the alert in the Html

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 const input = document.getElementById('input');
 const searchBtn=document.getElementById('btn');
 const notFoundWord=document.querySelector('.cant_find_word');
-const apiKey='';
 
 searchBtn.addEventListener('click',e =>{
     // console.log('Hello World!!')
@@ -21,7 +20,10 @@ async function getSearchedWord(searchedWord){
     const response = await fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${searchedWord}?key=696bb10c-9957-46a7-a71d-ebdc6368e23d`);
     const data = await response.json();
     console.log(data);
-    
-    
 
+    if (!data.length){
+        notFoundWord.innerText="No results Found";
+        return;
+    }
 }
+    
