1ccd94fad22857a6720b74dc96d46e138a92d01f
+ <+>
2022-12-11 14:26:25 +0300

    alert if the user enter an empty string

--- src/script.js
+++ src/script.js
@@ -4,4 +4,10 @@ const searchBtn=document.getElementById('btn');
 searchBtn.addEventListener('click',e =>{
     console.log('Hello World!!')
     e.preventDefault();
+
+    const searchedWord=input.value;
+    console.log(searchedWord);
+    if (searchedWord==="");
+    alert('Please enterd the searched Word');
+    return;
 })
