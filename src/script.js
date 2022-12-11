28df76f134db21328facccf50b6cfe158ec623d9
+ <+>
2022-12-11 13:42:59 +0300

    events for the searchBtn

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('input');
+const searchBtn=document.getElementById('btn');
+
+searchBtn.addEventListener('click',e =>{
+    console.log('Hello World!!')
+    e.preventDefault();
+})
